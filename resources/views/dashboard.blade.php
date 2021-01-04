f7373ac335fe5791fc5aac81ed8a6ae0b260c9f9
AmirReza Mansoorian <amirreza.mansoorian2003@gmail.com>
2021-01-04 21:15:24 +0330

    add more styles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -41,25 +41,27 @@
         </div>
         <div class="col-lg-10 col-md-9 col-12 h-12" id="mainpage">
             <div class="col-12 navbar-top">
-                <button id="collapser" class="btn btn-secondary">
-                    <i class="fas fa-angle-double-right"></i>
-                </button>
-                <button data-event="max" class="btn scr-controller">
-                    <i class="fas fa-expand-arrows-alt fa-compress"></i>
-                </button>
-                <div class="admin-info-container float-right">
-                    <a href="#"><img src="/images/amir.jpg" alt=""></a>
-                    <a href="#" class="mr-1 ml-1 admin-email-link">amirreza.mansoorian2003@gmail.com</a>
-                    <div class="btn-group drop-down-container">
-                        <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-user-tie"></i> Amir
-                        </button>
-                        <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#"><i class="fas fa-cog"></i> Setting</a>
-                        <a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt"></i> Exit</a>
+                <div class="admin-info-container float-left">
+                    <a href="#" class="float-left mr-2"><img src="/images/amir.jpg" alt="profile photo"></a>
+                    <div>
+                        <p class="admin-email-link">amirreza.mansoorian2003@gmail.com</p>
+                        <div class="btn-group drop-down-container">
+                            <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-user-tie"></i> Amir
+                            </button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">تنظیمات <i class="fas fa-cog"></i></a>
+                                <a class="dropdown-item" href="#">خروج <i class="fas fa-sign-out-alt"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <button data-event="max" class="btn scr-controller float-right">
+                    <i class="fas fa-expand-arrows-alt fa-compress"></i>
+                </button>
+                <button id="collapser" class="btn btn-secondary float-right">
+                    <i class="fas fa-angle-double-left"></i>
+                </button>
             </div>
             <div class="col-12 contentbar float-left">
                 @yield('pagecontent')
