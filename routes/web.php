1031b9be3cc0052c845f3905064e1f0aad938426
Yoonus <yoonustehrani28@gmail.com>
2021-01-04 19:52:00 +0330

    basic routes

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function() {
-
+Route::group(['prefix' => 'task-manager', 'as' => 'task-manager.', 'middleware' => ['auth']], function () {
+    Route::get('dashboard', function() {
+        return view('dashboard');
+    });
+    Route::resource('users', 'UserController');
+    Route::resource('roles', 'RoleController');
+    Route::resource('permissions', 'PermissionController');
+    Route::resource('proiorities', 'ProiorityController');
+    Route::resource('workspaces',  'WorkspaceController');
+    Route::resource('demands',     'DemandController');
+    Route::resource('tasks',       'TaskController');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
