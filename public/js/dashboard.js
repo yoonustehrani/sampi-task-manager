b94053128f1297d1a7078be99849354347821077
AmirReza Mansoorian <amirreza.mansoorian2003@gmail.com>
2021-01-08 08:30:48 +0330

    clear the directories

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -28601,10 +28601,10 @@ if (false) {} else {
 
 /***/ }),
 
-/***/ "./resources/js/react/components/Dashboard.js":
-/*!****************************************************!*\
-  !*** ./resources/js/react/components/Dashboard.js ***!
-  \****************************************************/
+/***/ "./resources/js/react/components/Dashboard/index.js":
+/*!**********************************************************!*\
+  !*** ./resources/js/react/components/Dashboard/index.js ***!
+  \**********************************************************/
 /*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -28882,14 +28882,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _components_Dashboard__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/Dashboard */ "./resources/js/react/components/Dashboard.js");
+/* harmony import */ var _components_Dashboard___WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/Dashboard/ */ "./resources/js/react/components/Dashboard/index.js");
 
 
 
 var target = document.getElementById('react-dashboard');
 
 if (target) {
-  react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Dashboard__WEBPACK_IMPORTED_MODULE_2__["default"], null), target);
+  react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Dashboard___WEBPACK_IMPORTED_MODULE_2__["default"], null), target);
 }
 
 /***/ }),
--- resources/js/react/dashboard.js
+++ resources/js/react/dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import Dashboard from './components/Dashboard'
+import Dashboard from './components/Dashboard/'
 
 const target = document.getElementById('react-dashboard')
 
