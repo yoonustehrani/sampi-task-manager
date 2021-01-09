1df9f6693f37e702795dd1a34e8c6d4593415f23
Yoonus <yoonustehrani28@gmail.com>
2021-01-09 05:30:25 +0330

    refactoring the counter api

--- app/Http/Controllers/Api/CounterController.php
+++ app/Http/Controllers/Api/CounterController.php
@@ -9,28 +9,23 @@ class CounterController extends Controller
 {
     public function workspaces(Request $request)
     {
-        return ['count' => $request->user()->workspaces()->count()];
+        return ['all' => $request->user()->workspaces()->count()];
     }
     public function tasks(Request $request)
     {
-        $count = 0;
         $user = $request->user();
-        if ($request->relationship && method_exists($user, $request->relationship . '_tasks')) {
-            $count = $user->{$request->relationship . '_tasks'}()->count();
-        } else {
-            $count = $user->tasks()->count();
-        }
-        return ['count' => $count];
+        return [
+            'finished'   => $user->finished_tasks()->count(),
+            'unfinished' => $user->unfinished_tasks()->count(),
+            'expired'    => $user->tasks()->whereNotNull('due_to')->where('due_to', '<', now('Asia/Tehran'))->count()
+        ];
     }
     public function demands(Request $request)
     {
-        $count = 0;
         $user = $request->user();
-        if ($request->relationship && method_exists($user, $request->relationship . '_demnds')) {
-            $count = $user->{$request->relationship . '_demnds'}()->count();
-        } else {
-            $count = $user->demands()->count();
-        }
-        return ['count' => $count];
+        return [
+            'finished'   => $user->demands()->whereNull('finished_at')->count(),
+            'unfinished' => $user->demands()->whereNotNull('finished_at')->count(),
+        ];
     }
 }
