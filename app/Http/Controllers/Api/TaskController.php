3bf026990f096d9c1e8d7e1c7d5a6a30d1c9e8fe
Yoonus <yoonustehrani28@gmail.com>
2021-01-09 04:18:10 +0330

    bug fixed

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -34,7 +34,7 @@ class TaskController extends Controller
             'order' => 'nullable|string',
             'order_by' => 'nullable|string'
         ]);
-        $user_tasks = $request->user()->tasks()->with('users')->withCount('demands');
+        $user_tasks = $request->user()->tasks()->with(['users', 'workspace'])->withCount('demands');
         if ($request->order_by) {
             $order = $request->order != 'desc' ? 'asc' : 'desc';
             $user_tasks = $user_tasks->orderBy($request->order_by, $order);
