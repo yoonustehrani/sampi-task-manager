5d1ac0bdcd48e6f79f1bc5a4e148018cf218327b
Yoonus <yoonustehrani28@gmail.com>
2021-01-09 05:53:51 +0330

    counter apis are in the index.blade.php now for Amir Jun

--- app/Http/Controllers/Api/CounterController.php
+++ app/Http/Controllers/Api/CounterController.php
@@ -9,23 +9,43 @@ class CounterController extends Controller
 {
     public function workspaces(Request $request)
     {
-        return ['all' => $request->user()->workspaces()->count()];
+        return [
+            'all' => [
+                'count' => $request->user()->workspaces()->count(),
+                'href'  => route('task-manager.workspaces.index')
+            ]
+        ];
     }
     public function tasks(Request $request)
     {
         $user = $request->user();
         return [
-            'finished'   => $user->finished_tasks()->count(),
-            'unfinished' => $user->unfinished_tasks()->count(),
-            'expired'    => $user->tasks()->whereNotNull('due_to')->where('due_to', '<', now('Asia/Tehran'))->count()
+            'finished' => [
+                'count' => $user->finished_tasks()->count(),
+                'href'  => '#'
+            ],
+            'unfinished' => [
+                'count' => $user->unfinished_tasks()->count(),
+                'href'  => '#'
+            ],
+            'expired' => [
+                'count' => $user->tasks()->whereNotNull('due_to')->where('due_to', '<', now('Asia/Tehran'))->count(),
+                'href'  => '#'
+            ]
         ];
     }
     public function demands(Request $request)
     {
         $user = $request->user();
         return [
-            'finished'   => $user->demands()->whereNull('finished_at')->count(),
-            'unfinished' => $user->demands()->whereNotNull('finished_at')->count(),
+            'finished'   => [
+                'count' => $user->demands()->whereNull('finished_at')->count(),
+                'href'  => '#'
+            ],
+            'unfinished' => [
+                'count' => $user->demands()->whereNotNull('finished_at')->count(),
+                'href'  => '#'
+            ],
         ];
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,12 +9,16 @@
 <body>
     <script src="{{ asset('js/app.js') }}"></script>
     <script>
-        let list_tasks = "{{ route('api.task-manager.tasks.index', ['workspace' => 1, 'api_token' => auth()->user()->api_token]) }}";
-        let add_task = "{{ route('api.task-manager.tasks.store', ['workspace' => 1, 'api_token' => auth()->user()->api_token]) }}";
+        // let list_tasks = "{{ route('api.task-manager.tasks.index', ['workspace' => 1, 'api_token' => auth()->user()->api_token]) }}";
+        // let add_task = "{{ route('api.task-manager.tasks.store', ['workspace' => 1, 'api_token' => auth()->user()->api_token]) }}";
         
+        let workspace_counter = "{{ route('api.task-manager.counter.workspaces', ['api_token' => auth()->user()->api_token]) }}"
+        let task_counter = "{{ route('api.task-manager.counter.tasks', ['api_token' => auth()->user()->api_token]) }}"
+        let demand_counter = "{{ route('api.task-manager.counter.demands', ['api_token' => auth()->user()->api_token]) }}"
+
         // Get tasks mixed
-        let mixed_tasks = "{{ route('api.task-manager.tasks.mixed', ['api_token' => auth()->user()->api_token]) }}";
-        mixed_tasks += "?limit=10" // &order_by=due_to&order=desc ----> works fine !
+        // let mixed_tasks = "{{ route('api.task-manager.tasks.mixed', ['api_token' => auth()->user()->api_token]) }}";
+        // mixed_tasks += "?limit=10" // &order_by=due_to&order=desc ----> works fine !
         // List tasks -> works fine !
         // axios.get(list_tasks).then(res => {
         //     console.log(res.data);
