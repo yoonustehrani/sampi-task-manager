cec4b5b24d57df6e5170a0cf4fe1b888a5721f06
Yoonus <yoonustehrani28@gmail.com>
2021-01-11 07:24:56 +0330

    unknown commit

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -92,7 +92,7 @@
                     {
                         label: 'وظایف',
                         // showLine: false,
-                        lineTension: 0,
+                        // lineTension: 0,
                         data: data,
                         // fill: false,
                         borderColor: 'red',
@@ -153,53 +153,6 @@
                 }
             }
         });
-        // new Chart(document.getElementById("chartjs-0"),{"type":"line","data":{"labels":["January","February","March","April","May","June","July"],"datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],"fill":false,"borderColor":"rgb(75, 192, 192)","lineTension":0.1}]},"options":{}});
-//         var myChart = new Chart(ctx, {
-//             type: 'line',
-//             data: {
-//                 labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//                 datasets: [{
-//                     label: '# of Votes',
-//                     data: [12, 19, 3, 5, 2, 3],
-//                     backgroundColor: [
-//                         'rgba(255, 99, 132, 0.4)',
-//                         // 'rgba(255, 99, 132, 0.1)',
-//                         // 'rgba(255, 99, 132, 0.1)',
-//                         // 'rgba(255, 99, 132, 0.1)',
-//                         // 'transparent'
-//                     ],
-//                     borderColor: [
-//                         'rgba(255, 99, 132, 1)',
-//                         'rgb(0,0,0)',
-//                         'rgb(0,0,0)',
-//                         'rgb(0,0,0)',
-//                         'rgb(0,0,0)',
-//                         'rgb(0,0,0)',
-//                     ],
-//                     borderWidth: 3
-//                 }]
-//             },
-//             options: {
-//                 legend: {
-//                     display: true,
-//                     labels: {
-//                         fontColor: 'rgb(255, 99, 132)'
-//                     }
-//                 },
-//                 title: {
-//                     display: true,
-//                     text: 'کردن کان'
-//                 },
-//                 responsiveAnimationDuration: 500,
-//                 scales: {
-//                     yAxes: [{
-//                         ticks: {
-//                             beginAtZero: true
-//                         }
-//                     }]
-//                 },
-//             }
-// });
     </script>
 </body>
 </html>
