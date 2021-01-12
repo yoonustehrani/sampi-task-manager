5ad59764424ac917568e96b3cbb05a344633144a
AmirReza Mansoorian <amirreza.mansoorian2003@gmail.com>
2021-01-12 04:45:58 +0330

    clean some codes

--- resources/views/theme/dashboard.blade.php
+++ resources/views/theme/dashboard.blade.php
@@ -19,6 +19,27 @@
 @push('scripts')
     <script>
         var APP_PATH = "{{ asset('/') }}";
+        var list_tasks = "{{ route('api.task-manager.tasks.index', ['workspace' => "workspaceId", 'api_token' => auth()->user()->api_token]) }}";
+        var add_task = "{{ route('api.task-manager.tasks.store', ['workspace' => "workspaceId", 'api_token' => auth()->user()->api_token]) }}";
+        {{-- axios.post(add_task, {
+            title: 'Task number 1',
+            priority: 1,
+            group: 'fictional',
+            users: [
+                '100',
+                '500'
+            ]
+        }).then(res => {
+            console.log(res.data);
+        }).catch(e => {
+            let {status, data} = e.response;
+            if (status === 422) {
+                let {errors} = data;
+                Object.entries(errors).map(([param, message]) => {
+                    console.log(message);
+                })
+            }
+        }) --}}
     </script>
     <script src="{{ asset('/js/dashboard.js') }}"></script>
 @endpush
