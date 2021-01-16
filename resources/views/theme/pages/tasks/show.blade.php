71147e60e1402594639c7bb7f91a8408cf0b7dc8
Yoonus <yoonustehrani28@gmail.com>
2021-01-16 05:15:51 +0330

    added task show api to show.blade

--- resources/views/theme/pages/tasks/show.blade.php
+++ resources/views/theme/pages/tasks/show.blade.php
@@ -5,5 +5,6 @@
 @endsection
 
 @section('page-content')
-    
+<p>Example : </p>
+<p>{{ route('api.task-manager.tasks.show', ['workspace' => $task->workspace->id, 'task' => $task->id, 'api_token' => auth()->user()->api_token]) }}</p>
 @endsection
