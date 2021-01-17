27e385ec3f6d19acd5f2095f3d53e7164fffff91
AmirReza Mansoorian <amirreza.mansoorian2003@gmail.com>
2021-01-17 14:45:12 +0330

    hellooo

--- resources/js/react/components/tinymce-editor/index.js
+++ resources/js/react/components/tinymce-editor/index.js
@@ -16,19 +16,16 @@ export default class TinymcEditor extends React.Component {
                     skin_url: '/css',
                     height: 250,
                     menubar: false, // make true for more options like image insert
-                    plugins: [
-                        'advlist autolink lists link preview wordcount paste help code',
-                    ],
-                    toolbar:
-                    'undo redo | formatselect | bold italic backcolor | \
-                    alignleft aligncenter alignright alignjustify | \
-                    bullist numlist outdent indent | removeformat | code | help'
+                    // plugins: [`
+                    //     'advlist autolink lists link preview wordcount paste help code',
+                    // ],
+                    // toolbar:
+                    // 'undo redo | formatselect | bold italic backcolor | \
+                    // alignleft aligncenter alignright alignjustify | \
+                    // bullist numlist outdent indent | removeformat | code | help'
                 }}
                 onEditorChange={this.handleEditorChange}
             />
         );
     }
 }
-
-export default TinymcEditor;
-
