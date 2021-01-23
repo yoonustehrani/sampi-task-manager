4c8796d878018f02b48e701209b7360fcb9541c2
Yoonus <yoonustehrani28@gmail.com>
2021-01-23 20:18:21 +0330

    refactoring seeder code

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,17 +13,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RoleSeeder::class);
-        $this->call(PermissionSeeder::class);
-        $this->call(PrioritySeeder::class);
-        $user = new User;
-        $user->first_name = 'یوزر';
-        $user->name = 'elnovel';
-        $user->email = 'elnovelofficial@gmail.com';
-        $user->password = bcrypt('hello123');
-        $user->api_token = \Illuminate\Support\Str::uuid();
-        $user->save();
-        $roles = Role::all();
-        $user->roles()->attach($roles);
+        // $this->call(RoleSeeder::class);
+        // $this->call(PermissionSeeder::class);
+        // $this->call(PrioritySeeder::class);
+        // $user = new User;
+        // $user->first_name = 'یوزر';
+        // $user->name = 'elnovel';
+        // $user->email = 'elnovelofficial@gmail.com';
+        // $user->password = bcrypt('hello123');
+        // $user->api_token = \Illuminate\Support\Str::uuid();
+        // $user->save();
+        // $roles = Role::all();
+        // $user->roles()->attach($roles);
+        (new \App\Permission())->insert([
+            [
+                'key' => 'can_edit_user_role',
+                'label' => 'ویرایش سمت کاربران'
+            ],
+            [
+                'key' => 'can_manage_system',
+                'label' => 'مدیریت سیستم'
+            ]
+        ]);
+        $role = \App\Role::whereName('developer')->first();
+        $role->permissions()->sync(\App\Permission::all());
     }
 }
