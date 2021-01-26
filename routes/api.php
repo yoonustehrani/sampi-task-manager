9d196f4451b25ffc1dc9214aac0eee2003a42793
Yoonus <yoonustehrani28@gmail.com>
2021-01-26 11:26:20 +0330

    cleaning up

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::group(['prefix' => 'task-manager', 'as' => 'api.task-manager.'], function () {
     Route::get('priorities', function() {
         return \App\Priority::all();
