3b984df37e040d44ff4a3f5ad8a89a72e2b10020
Yoonus <yoonustehrani28@gmail.com>
2021-02-06 14:09:22 +0330

    welcome page finished

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,25 @@
     <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
 </head>
 <body>
-    
+    <div class="col-12 h-100 float-left main-division">
+        <div class="col-md-6 col-12 float-left mb-md-0 mb-3 p-3 left-section">
+            <div class="logo-section">
+                <img src="{{ asset('images/logo/sampi.png') }}" alt="">
+                <p class="m-0">Sampi Tech Group</p>
+            </div>
+        </div>
+        <div class="col-md-6 col-12 float-left mb-md-0 mb-3 p-3 right-section">
+            <p class="col-12 float-left text-center"><i class="fas fa-list"></i> ماژول ها</p>
+            <div class="float-left module-section">
+                <ul class="col-md-6 col-12 p-0 module-list float-right">
+                    <li><a href="{{ route('task-manager.') }}"><i class="fas fa-briefcase"></i> مدیریت یکپارچه پروژه ها</a></li>
+                </ul>
+                <ul class="col-md-6 col-12 p-0 module-list float-right">
+                    {{-- <li><a href="{{ route('task-manager.') }}">سیستم یکپارچه مدیریت پروژه</a></li> --}}
+                </ul>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
 
