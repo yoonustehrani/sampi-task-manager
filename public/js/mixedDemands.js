b10c89ebfb2c454b72725c83ad3ea3cf704e9922
AmirReza Mansoorian <amirreza.mansoorian2003@gmail.com>
2021-02-09 04:35:07 +0330

    fix bug

--- public/js/mixedDemands.js
+++ public/js/mixedDemands.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -57825,7 +57825,7 @@ renderWithImg("#new-task-project-select", "پروژه مربوطه را انتخ
 
 /***/ }),
 
-/***/ 1:
+/***/ 2:
 /*!**************************************************!*\
   !*** multi ./resources/js/react/mixedDemands.js ***!
   \**************************************************/
