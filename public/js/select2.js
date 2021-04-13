a082597c9e8a093813dd2b0d3e9a7830fbfb813e
AmirReza Mansoorian <amirreza.mansoorian2003@gmail.com>
2021-04-13 05:21:52 +0430

    new cahnges

--- public/js/select2.js
+++ public/js/select2.js
@@ -1,1 +1,266 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var a=t[r]={i:r,l:!1,exports:{}};return e[r].call(a.exports,a,a.exports,n),a.l=!0,a.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var a in e)n.d(r,a,function(t){return e[t]}.bind(null,a));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=0)}({0:function(e,t,n){n("38i8"),n("pyCd"),n("xdt5"),n("v8n2"),e.exports=n("9SFV")},"38i8":function(e,t,n){"use strict";n.r(t),n.d(t,"formatOptionWithIcon",(function(){return r})),n.d(t,"formatOptionWithImage",(function(){return a})),n.d(t,"formatOption",(function(){return c})),n.d(t,"simpleSearch",(function(){return l})),n.d(t,"renderWithImg",(function(){return i}));var r=function(e){if(e.element){var t=e.element.attributes.icon_name.nodeValue,n=e.element.attributes.container_class?e.element.attributes.container_class.nodeValue:null;return $('<div class="select-option '.concat(null!==n?n:"",'"><i class="').concat(t,'"></i>').concat(e.text,"</div>"))}},a=function(e){if(e.element){var t=e.element.attributes.img_address.nodeValue?e.element.attributes.img_address.nodeValue:console.log("kirsag"),n=e.element.attributes.is_admin;return $('\n            <div class="select-option circle-avatar-pic">\n                <img class="ml-1" src="'.concat(t,'"/>\n                ').concat(e.text,"\n                ").concat(void 0!==n?'<span class="badge badge-pill mr-1 '.concat("1"===n.nodeValue?"badge-success":"badge-primary",'">').concat("1"===n.nodeValue?"ادمین":"کاربر","</span>"):"","  \n            </div>\n        "))}},c=function(e){return $('\n        <div class="select-option">\n            '.concat(e.workspace?'<div class="circle-avatar-pic small-avatar mb-1"><img src="'.concat(APP_PATH+e.workspace.avatar_pic,'"/><span class="badge badge-light mr-1">').concat(e.workspace.title,"</span></div>"):"","\n            ").concat(e.text).concat(e.group?" (".concat(e.group,")"):"","\n        </div>\n    "))};$("#new-task-priority, #tasks_order_select, #tasks_order_by_select, #tasks_relation_select, #mixed_tasks_order_select, #mixed_tasks_order_by_select, #mixed_tasks_relation_select, #mixed_demands_order_select, #mixed_demands_order_by_select, #mixed_demands_relation_select, #mixed_needs_order_select, #mixed_needs_order_by_select, #mixed_needs_relation_select, #mixed_all_order_select, #mixed_all_order_by_select, #mixed_all_relation_select").select2({templateResult:r,minimumResultsForSearch:1/0,width:"100%",dir:"rtl",language:{searching:function(){return"درحال جستجو ..."},noResults:function(){return"نتیجه ای یافت نشد"}}}),$(".select2-search__field").css("width","100%");var l=function(e,t){var n=arguments.length>2&&void 0!==arguments[2]?arguments[2]:$("#new-demand-project-select").val();$(e).select2({templateResult:c,templateSelection:function(e,t){return $(e.element).attr("workspace_id",e.workspace_id),e.text},placeholder:"کار مربوطه را جستجو و انتخاب کنید",width:"100%",dir:"rtl",minimumInputLength:3,delay:250,ajax:{url:"undefined"!=typeof simple_search_url?simple_search_url:"",data:function(e){return{q:e.term,workspace:n,parentOnly:t}},processResults:function(e){return{results:$.map(e,(function(e){return e.text=e.text||e.title,e}))}}},language:{searching:function(){return"درحال جستجو ..."},noResults:function(){return"نتیجه ای یافت نشد"}},allowClear:!0})};l("#task-select",!1),l("#parent-task-select",!0);var i=function(e,t,n){$(e).select2({templateResult:a,placeholder:t,width:"100%",dir:"rtl",multiple:n,language:{searching:function(){return"درحال جستجو ..."},noResults:function(){return"نتیجه ای یافت نشد"}},allowClear:!0})};i("#new-demand-member","نیاز به کمک چه کسی دارید؟",!1),i("#new-demand-project-select","پروژه مربوطه را انتخاب کنید",!1),i("#new-task-members","انجام دهندگان این کار",!0),i("#new-task-project-select","پروژه مربوطه را انتخاب کنید",!1)},"9SFV":function(e,t){},pyCd:function(e,t){},v8n2:function(e,t){},xdt5:function(e,t){}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/select2.js":
+/*!*********************************!*\
+  !*** ./resources/js/select2.js ***!
+  \*********************************/
+/*! exports provided: formatOptionWithIcon, formatOptionWithImage, formatOption, simpleSearch, renderWithImg */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "formatOptionWithIcon", function() { return formatOptionWithIcon; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "formatOptionWithImage", function() { return formatOptionWithImage; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "formatOption", function() { return formatOption; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "simpleSearch", function() { return simpleSearch; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "renderWithImg", function() { return renderWithImg; });
+var formatOptionWithIcon = function formatOptionWithIcon(option) {
+  if (option.element) {
+    var icon_name = option.element.attributes.icon_name.nodeValue;
+    var containerClass = option.element.attributes.container_class ? option.element.attributes.container_class.nodeValue : null;
+    return $("<div class=\"select-option ".concat(containerClass !== null ? containerClass : "", "\"><i class=\"").concat(icon_name, "\"></i>").concat(option.text, "</div>"));
+  }
+};
+var formatOptionWithImage = function formatOptionWithImage(option) {
+  if (option.element) {
+    var img_src = option.element.attributes.img_address.nodeValue ? option.element.attributes.img_address.nodeValue : console.log('kirsag');
+    var is_user_admin = option.element.attributes.is_admin;
+    return $("\n            <div class=\"select-option circle-avatar-pic\">\n                <img class=\"ml-1\" src=\"".concat(img_src, "\"/>\n                ").concat(option.text, "\n                ").concat(typeof is_user_admin !== "undefined" ? "<span class=\"badge badge-pill mr-1 ".concat(is_user_admin.nodeValue === "1" ? "badge-success" : "badge-primary", "\">").concat(is_user_admin.nodeValue === "1" ? "ادمین" : "کاربر", "</span>") : "", "  \n            </div>\n        "));
+  }
+};
+var formatOption = function formatOption(option) {
+  return $("\n        <div class=\"select-option\">\n            ".concat(option.workspace ? "<div class=\"circle-avatar-pic small-avatar mb-1\"><img src=\"".concat(APP_PATH + option.workspace.avatar_pic, "\"/><span class=\"badge badge-light mr-1\">").concat(option.workspace.title, "</span></div>") : "", "\n            ").concat(option.text).concat(option.group ? " (".concat(option.group, ")") : "", "\n        </div>\n    "));
+};
+$('#new-task-priority, #tasks_order_select, #tasks_order_by_select, #tasks_relation_select, #mixed_tasks_order_select, #mixed_tasks_order_by_select, #mixed_tasks_relation_select, #mixed_demands_order_select, #mixed_demands_order_by_select, #mixed_demands_relation_select, #mixed_needs_order_select, #mixed_needs_order_by_select, #mixed_needs_relation_select, #mixed_all_order_select, #mixed_all_order_by_select, #mixed_all_relation_select').select2({
+  templateResult: formatOptionWithIcon,
+  minimumResultsForSearch: Infinity,
+  width: '100%',
+  dir: "rtl",
+  language: {
+    searching: function searching() {
+      return "درحال جستجو ...";
+    },
+    noResults: function noResults() {
+      return "نتیجه ای یافت نشد";
+    }
+  }
+});
+$('.select2-search__field').css('width', '100%');
+var simpleSearch = function simpleSearch(ids, parentOnly) {
+  var workspaceId = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : $("#new-demand-project-select").val();
+  $(ids).select2({
+    templateResult: formatOption,
+    templateSelection: function templateSelection(data, container) {
+      $(data.element).attr('workspace_id', data.workspace_id);
+      return data.text;
+    },
+    placeholder: 'کار مربوطه را جستجو و انتخاب کنید',
+    width: "100%",
+    dir: "rtl",
+    minimumInputLength: 3,
+    delay: 250,
+    ajax: {
+      url: typeof simple_search_url !== "undefined" ? simple_search_url : "",
+      data: function data(params) {
+        return {
+          q: params.term,
+          workspace: workspaceId,
+          parentOnly: parentOnly
+        };
+      },
+      processResults: function processResults(res) {
+        var data = $.map(res, function (obj) {
+          obj.text = obj.text || obj.title; // replace name with the property used for the text
+
+          return obj;
+        });
+        return {
+          results: data
+        };
+      }
+    },
+    language: {
+      searching: function searching() {
+        return "درحال جستجو ...";
+      },
+      noResults: function noResults() {
+        return "نتیجه ای یافت نشد";
+      }
+    },
+    allowClear: true
+  });
+};
+simpleSearch('#task-select', false);
+simpleSearch("#parent-task-select", true);
+var renderWithImg = function renderWithImg(ids, placeholder, multiple) {
+  $(ids).select2({
+    templateResult: formatOptionWithImage,
+    placeholder: placeholder,
+    width: "100%",
+    dir: "rtl",
+    multiple: multiple,
+    language: {
+      searching: function searching() {
+        return "درحال جستجو ...";
+      },
+      noResults: function noResults() {
+        return "نتیجه ای یافت نشد";
+      }
+    },
+    allowClear: true
+  });
+};
+renderWithImg("#new-demand-member", "نیاز به کمک چه کسی دارید؟", false);
+renderWithImg("#new-demand-project-select", "پروژه مربوطه را انتخاب کنید", false);
+renderWithImg("#new-task-members", "انجام دهندگان این کار", true);
+renderWithImg("#new-task-project-select", "پروژه مربوطه را انتخاب کنید", false);
+
+/***/ }),
+
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./resources/sass/auth.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/auth.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./resources/sass/tinytheme.scss":
+/*!***************************************!*\
+  !*** ./resources/sass/tinytheme.scss ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./resources/sass/welcome.scss":
+/*!*************************************!*\
+  !*** ./resources/sass/welcome.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!**********************************************************************************************************************************************************!*\
+  !*** multi ./resources/js/select2.js ./resources/sass/app.scss ./resources/sass/welcome.scss ./resources/sass/tinytheme.scss ./resources/sass/auth.scss ***!
+  \**********************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! I:\projects\task-manager\resources\js\select2.js */"./resources/js/select2.js");
+__webpack_require__(/*! I:\projects\task-manager\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! I:\projects\task-manager\resources\sass\welcome.scss */"./resources/sass/welcome.scss");
+__webpack_require__(/*! I:\projects\task-manager\resources\sass\tinytheme.scss */"./resources/sass/tinytheme.scss");
+module.exports = __webpack_require__(/*! I:\projects\task-manager\resources\sass\auth.scss */"./resources/sass/auth.scss");
+
+
+/***/ })
+
+/******/ });
